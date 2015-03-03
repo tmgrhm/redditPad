70cfca177bf2b0b70bb94ea30c0008d123cbe127
Tom Graham <tom@Toms-Air.lan>
2015-03-03 19:35:35 +0000

    Initial Commit

---
+++ Bluit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bluit
+//
+//  Created by Tom Graham on 03/03/2015.
+//  Copyright (c) 2015 Tom Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bluit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bluit
+//
+//  Created by Tom Graham on 03/03/2015.
+//  Copyright (c) 2015 Tom Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
