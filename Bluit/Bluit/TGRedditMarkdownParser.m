1618f272e3c2bc34905a5eeaaef6785f18a44eeb
tmgrhm <Tom@Toms-Mac-mini.lan>
2015-05-16 11:29:14 +0100

    Start of attempt to style attributed strings from HTML myself (unused atm)

--- Bluit/Bluit/TGRedditMarkdownParser.m
+++ Bluit/Bluit/TGRedditMarkdownParser.m
@@ -27,16 +27,11 @@
 	parser.linkFontName = @"AvenirNext-DemiBold";
 	parser.topAttributes = @{NSForegroundColorAttributeName : [ThemeManager colorForKey:kTGThemeTextColor]};
 	
-	NSMutableAttributedString *string = [[parser attributedStringFromMarkdownString:markdown] mutableCopy]; // TODO I think XNG allows you to set paragraph style on the parser instead
-	NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
-	[paragraphStyle setMinimumLineHeight:21.0];
-	[paragraphStyle setParagraphSpacing:6.0];
+	NSAttributedString *attrStr = [parser attributedStringFromMarkdownString:markdown]; // TODO I think XNG allows you to set paragraph style on the parser instead
 	
-	[string addAttribute:NSParagraphStyleAttributeName
-				   value:paragraphStyle
-				   range:NSMakeRange(0, string.length)];
+	attrStr = [self styleAttributedString:attrStr];
 	
-	return string;
+	return attrStr;
 }
 
 + (NSAttributedString *) attributedStringFromHTML:(NSString *)html
@@ -44,10 +39,13 @@
 	html = [self htmlStringFromEscapedHTML:html];
 	html = [self styleHTML:html];
 	
-	NSAttributedString *attrStr = [[NSAttributedString alloc] initWithData:[html dataUsingEncoding:NSUTF8StringEncoding]
+	NSData *htmlData = [html dataUsingEncoding:NSUTF8StringEncoding];
+	NSAttributedString *attrStr = [[NSAttributedString alloc] initWithData:htmlData
 																   options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType }
 														documentAttributes:NULL error:NULL];
-	attrStr = [attrStr attributedSubstringFromRange:NSMakeRange(0, attrStr.length-1)];
+
+//	attrStr = [self styleAttributedString:attrStr];
+	attrStr = [attrStr attributedSubstringFromRange:NSMakeRange(0, attrStr.length-1)];  // trim trailing newline
 	
 	return attrStr;
 }
@@ -87,6 +85,70 @@
 	bodyHTML = [wrapperHTML stringByReplacingOccurrencesOfString:@"<!-- BODY -->" withString:bodyHTML];
 	
 	return bodyHTML;
+}
+
++ (NSAttributedString *) styleAttributedString:(NSAttributedString *)attributedString // TODO set line height while parsing lists and shit?
+{
+	CGFloat const fontSize = 15.0f;
+	CGFloat const minLineheight = fontSize * 1.4;
+	CGFloat const paragraphSpacing = fontSize * 0.4;
+	CGFloat const firstTab = fontSize * 0.8;
+	CGFloat const secondTab = fontSize * 2.3;
+	
+	NSMutableAttributedString *mutAttrStr = [attributedString mutableCopy];
+	
+	NSMutableParagraphStyle *bodyStyle = [NSMutableParagraphStyle new];
+	[bodyStyle setMinimumLineHeight:minLineheight];
+	[bodyStyle setParagraphSpacing:paragraphSpacing];
+	
+	[mutAttrStr addAttribute:NSParagraphStyleAttributeName
+					   value:bodyStyle
+					   range:NSMakeRange(0, mutAttrStr.length)];
+	
+	
+	// style lists
+	NSMutableParagraphStyle *listStyle = [bodyStyle mutableCopy];
+	listStyle.paragraphSpacingBefore = fontSize * 0.2;
+	listStyle.paragraphSpacing = fontSize * 0.6;
+	listStyle.headIndent = secondTab;
+	NSTextTab *listTab = [[NSTextTab alloc] initWithTextAlignment:NSTextAlignmentNatural
+														 location:firstTab
+														  options:nil];
+	NSTextTab *listTab2 = [[NSTextTab alloc] initWithTextAlignment:NSTextAlignmentNatural
+														 location:secondTab
+														  options:nil];
+	listStyle.tabStops = @[listTab, listTab2];
+	
+/**/
+	NSInteger index = 0;
+	NSMutableArray *listLineRanges = [NSMutableArray new];
+	NSArray *lines = [[mutAttrStr string] componentsSeparatedByString:@"\n"];
+	for (NSString *line in lines)
+	{
+		if ([line hasPrefix:@"\t"])
+		{
+			NSRange listLineRange = {index, line.length}; // subtract 1 from line length because we'll trim the ending \n
+			[listLineRanges addObject:[NSValue valueWithRange:listLineRange]];
+		}
+		index += line.length + 1; // add extra one for missing \n
+	}
+	
+	for (NSValue *rangeValue in listLineRanges) [mutAttrStr addAttribute:NSParagraphStyleAttributeName value:listStyle range:[rangeValue rangeValue]];
+	
+	/*
+	NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\n\t.*" options:kNilOptions error:nil];
+	
+	NSRange range = NSMakeRange(0, [mutAttrStr string].length);
+	
+	[regex enumerateMatchesInString:[mutAttrStr string] options:kNilOptions range:range usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+		
+		NSRange subStringRange = [result rangeAtIndex:1];
+		[mutableAttributedString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:subStringRange];
+	}];*/
+	
+	NSAttributedString *result = [mutAttrStr attributedSubstringFromRange:NSMakeRange(0, mutAttrStr.length-1)];  // trim extra newline
+	
+	return result;
 }
 
 @end
